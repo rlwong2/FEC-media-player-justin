a3bdee10ecf019ef7874c76a54a3aca3d64d307c
Justin Calvo <justin_calvo@hotmail.com>
2020-03-16 11:15:44 -0400

    Several changes in this. I updated the server side get request to pull one random item out when the request is made. I also added some data to database to test that this was working. I added style modueles to really start making this component look like soundcloud. I added momentjs to the project to have the correct display of time as well.

---
+++ client/src/css-modules/Container.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export default styled.div`
+  background-image: linear-gradient(to top, #09203f 0%, #537895 100%);
+  height: 380px;
+  width: 1240px;
+  margin: auto;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+`
---
+++ client/src/css-modules/Date-Genre.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export default styled.p`
+float: right;
+color: #fff;
+padding-right: 25px;
+margin-top: 30px;
+text-align: right;
+font-size: 16px;
+font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;
+`
